36e1d43f91c4764e222bfc9b8cc05b0d45d3b7d7
Vitororigens <vitorabreufjb@hotmail.com>
2020-02-18 17:05:43 -0300

    ajustando funcoes.js

--- js/funcoes.js
+++ js/funcoes.js
@@ -4,3 +4,13 @@ var dy = today.getDate();
 var mt = today.getMonth();
 var yr = today.getFullYear();
 document.getElementById('data').value = dy + "-" + mt + "-" + yr;
+
+//dropdown do usuario
+
+function navUser() {
+    if (document.getElementById('mostrarUser').style.display == 'none') {
+        document.getElementById('mostrarUser').style.display = 'block'
+    } else {
+        document.getElementById("mostrarUser").style.display = 'none'
+    }
+}
